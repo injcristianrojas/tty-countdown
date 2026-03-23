2749fa3efac504d798f8e270aa2015b22717e52f
Cristián Rojas <injcristianrojas@gmail.com>
2026-03-23 13:53:40 -0300

    Remainder time fix 00:00:00

--- functions/countdown.py
+++ functions/countdown.py
@@ -7,8 +7,8 @@ def get_times(end_time: str):
     end_hour, end_minute = map(int, end_time.split(":"))
     end_dt = now.replace(hour=end_hour, minute=end_minute, second=0)
 
-    if end_dt <= now:
-        end_dt += timedelta(days=1)
+    # if end_dt <= now:
+    #    end_dt += timedelta(days=1)
 
     time_left = end_dt - now
     total_seconds = int(time_left.total_seconds())
@@ -16,4 +16,8 @@ def get_times(end_time: str):
     hours, remainder = divmod(total_seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
 
-    return now.strftime("%H:%M:%S"), f"{hours:02}:{minutes:02}:{seconds:02}"
+    remainder_string = (
+        f"{hours:02}:{minutes:02}:{seconds:02}" if total_seconds > 0 else "00:00:00"
+    )
+
+    return now.strftime("%H:%M:%S"), remainder_string
